78f4a4ab23649d2845d80f4e55c58b47913b8e12
qimeng_zhang <qimeng_zhang@intsig.net>
2024-03-05 11:34:48 +0800

    feat: 晚上xterm

--- src/app/components/terminal.tsx
+++ src/app/components/terminal.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { ITheme, Terminal } from "@xterm/xterm";
-import { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { CanvasAddon } from "@xterm/addon-canvas";
 import "@xterm/xterm/css/xterm.css";
 import { FitAddon } from "@xterm/addon-fit";
 import { ImageAddon, IImageAddonOptions } from "@xterm/addon-image";
 import { WebLinksAddon } from "@xterm/addon-web-links";
+import { allPosts } from "contentlayer/generated";
+import { siteConfig } from "@/config";
 
-const initMessage = [
+const message = [
   "$ \x1b[31;1m404 Not Found - \x1b[1m访问路径不存在\x1b[0m",
   "",
-  "$ \x1b[31;1m试试访问的其他路径~\x1b[0m",
+  "$ \x1b[31;1m试试点击下面的其他路径~\x1b[0m",
   "",
   "$ \x1b[33mPost-博客\x1b[0m \x1b[36mAbout-关于作者\x1b[0m",
 ].join("\n\r");
@@ -50,33 +52,105 @@ function prompt(terminal: Terminal) {
   terminal.write("\r\n$ ");
 }
 const commands: Record<string, any> = {
-  help: {
+  ls: {
     f: (term: Terminal) => {
+      const colors = [`\x1b[32m`];
       term.writeln(
-        ["Welcome to xterm.js! Try some of the commands below."].join("\n\r")
+        allPosts
+          .map((item, index) => {
+            return [
+              "",
+              `${colors[index % colors.length]} ${item.title}\x1b[0m`,
+            ];
+          })
+          .flat(1)
+          .join("\r\n")
       );
       prompt(term);
     },
-    description: "输出help信息",
+    description: "查看全部文章列表",
+  },
+  clear: {
+    f: (term: Terminal) => {
+      term.write("\x1b[2J\x1b[3J\x1b[H");
+      prompt(term);
+    },
+    description: "清空全部输出",
+  },
+  help: {
+    f: (term: Terminal) => {
+      const padding = 10;
+      function formatMessage(name: string, description: string) {
+        const maxLength = term.cols - padding - 3;
+        let remaining = description;
+        const d = [];
+        while (remaining.length > 0) {
+          // Trim any spaces left over from the previous line
+          remaining = remaining.trimStart();
+          // Check if the remaining text fits
+          if (remaining.length < maxLength) {
+            d.push(remaining);
+            remaining = "";
+          } else {
+            let splitIndex = -1;
+            // Check if the remaining line wraps already
+            if (remaining[maxLength] === " ") {
+              splitIndex = maxLength;
+            } else {
+              // Find the last space to use as the split index
+              for (let i = maxLength - 1; i >= 0; i--) {
+                if (remaining[i] === " ") {
+                  splitIndex = i;
+                  break;
+                }
+              }
+            }
+            d.push(remaining.substring(0, splitIndex));
+            remaining = remaining.substring(splitIndex);
+          }
+        }
+        const message =
+          `  \x1b[36;1m${name.padEnd(padding)}\x1b[0m ${d[0]}` +
+          d.slice(1).map((e) => `\r\n  ${" ".repeat(padding)} ${e}`);
+        return message;
+      }
+      term.writeln(
+        [
+          "",
+          `欢迎来到${siteConfig.domain}! 试试下面的指令～`,
+          "",
+          ...Object.keys(commands).map((e) =>
+            formatMessage(e, commands[e].description)
+          ),
+        ].join("\n\r")
+      );
+      prompt(term);
+    },
+    description: "查看帮助信息",
   },
 };
 function runCommand(terminal: Terminal, command: string) {
   const cmd = command.trim().split(" ")[0];
   if (cmd.length > 0) {
     terminal.writeln("");
-    if (command in commands) {
-      commands[command].f(terminal);
+    if (command.toLocaleLowerCase() in commands) {
+      commands[command.toLocaleLowerCase()].f(terminal);
       return;
     }
     terminal.writeln(`${command}: command not found - 未发现指令`);
   }
   prompt(terminal);
 }
-export default function TerminalComponent() {
+interface TerminalComponentProps {
+  initMessage?: string;
+}
+const TerminalComponent: React.FC<TerminalComponentProps> = ({
+  initMessage = message,
+}) => {
   const terminalRef = useRef<HTMLDivElement>(null);
   const terminal = useRef<Terminal | null>(null);
   const fitAddon = useRef(new FitAddon());
-  const initTerminal = () => {
+  const initTerminal = useCallback(() => {
     terminal.current = new Terminal({
       convertEol: true, //启用时，光标将设置为下一行的开头
       scrollback: 100, //终端中的回滚量
@@ -96,7 +170,7 @@ export default function TerminalComponent() {
     term.open(terminalRef.current!);
     term.loadAddon(new CanvasAddon());
     term.writeln(initMessage);
-    term.writeln("\r\n$ 试试输入`help`查看其他指令");
+    term.writeln("\r\n$ 此外你还可以输入`help`查看帮助信息");
     prompt(term!);
     term.focus();
     let command = "";
@@ -130,7 +204,7 @@ export default function TerminalComponent() {
           }
       }
     });
-  };
+  }, [initMessage]);
   useEffect(() => {
     initTerminal();
     const fitAddonRef = fitAddon.current;
@@ -146,7 +220,7 @@ export default function TerminalComponent() {
       fitAddonRef.dispose();
       terminal.current?.dispose();
     };
-  }, []);
+  }, [initTerminal]);
   return (
     <div className="px-[8px] w-full h-full relative overflow-y-auto flex items-center justify-center">
       <div className="mx-auto max-w-xl px-[8px] py-[8px] bg-[#2D2E2C] rounded-md">
@@ -154,4 +228,5 @@ export default function TerminalComponent() {
       </div>
     </div>
   );
-}
+};
+export default TerminalComponent;
